42d31cfdf0145b2e93547ad0421edd3d57f7be17
Raftouch <rafanelkin@gmail.com>
2023-05-08 17:20:30 +0200

    :rocket: authorization (login) part done on backend

--- client/src/pages/AuthPage.jsx
+++ client/src/pages/AuthPage.jsx
@@ -26,6 +26,13 @@ export const AuthPage = () => {
     } catch (error) {}
   };
 
+  const loginHandler = async () => {
+    try {
+      const data = await request("/api/auth/login", "POST", { ...form });
+      message(data.message);
+    } catch (error) {}
+  };
+
   return (
     <div className="row">
       <div className="col s6 offset-s3">
@@ -62,6 +69,7 @@ export const AuthPage = () => {
             <button
               className="btn yellow darken-4"
               style={{ marginRight: 10 }}
+              onClick={loginHandler}
               disabled={loading}
             >
               Login
