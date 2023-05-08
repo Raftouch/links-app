ff82a53a1973ca337aa72dec68de09ba117d7f20
Raftouch <rafanelkin@gmail.com>
2023-05-08 15:49:01 +0200

    :wrench: added proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      include: "**/*.jsx",
-    }),
-  ],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        secure: false,
+      },
+    },
+  },
+  plugins: [react()],
 });
