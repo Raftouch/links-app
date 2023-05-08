449cb2024e3d492a3be9f4329c19d343366d521a
Raftouch <rafanelkin@gmail.com>
2023-05-08 14:57:57 +0200

    :sparkles: added new hook, auth page ready

--- client/src/pages/AuthPage.jsx
+++ client/src/pages/AuthPage.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHttp } from "../hooks/http.hook";
 
 export const AuthPage = () => {
+  const { loading, request } = useHttp();
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const changeHandler = (event) => {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
+
+  const registerHandler = async () => {
+    try {
+      const data = await request("/api/auth/register", "POST", { ...form });
+      console.log("Data", data);
+    } catch (error) {}
+  };
+
   return (
     <div className="row">
       <div className="col s6 offset-s3">
@@ -11,31 +29,43 @@ export const AuthPage = () => {
             <div>
               <div className="input-field">
                 <input
-                //   placeholder="Enter valid email"
+                  //   placeholder="Enter valid email"
                   id="email"
                   type="email"
                   name="email"
                   className="orange-input"
+                  onChange={changeHandler}
                 />
                 <label htmlFor="email">Email</label>
               </div>
               <div className="input-field">
                 <input
-                //   placeholder="Enter valid email"
+                  //   placeholder="Enter valid email"
                   id="password"
                   type="password"
                   name="password"
                   className="orange-input"
+                  onChange={changeHandler}
                 />
                 <label htmlFor="email">Password</label>
               </div>
             </div>
           </div>
           <div className="card-action">
-            <button className="btn yellow darken-4" style={{ marginRight: 10 }}>
+            <button
+              className="btn yellow darken-4"
+              style={{ marginRight: 10 }}
+              disabled={loading}
+            >
               Login
             </button>
-            <button className="btn grey lighten-1 black-text">Register</button>
+            <button
+              className="btn grey lighten-1 black-text"
+              onClick={registerHandler}
+              disabled={loading}
+            >
+              Register
+            </button>
           </div>
         </div>
       </div>
