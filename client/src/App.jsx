a16059ffbf4d0691f6a51b5b659b70d8a8d84a83
Raftouch <rafanelkin@gmail.com>
2023-05-09 17:05:40 +0200

    :sparkles: created authorization hook (login, logout)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import "materialize-css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { useRoutes } from "./routes/Routes";
+import { useAuth } from "./hooks/auth.hook";
 
 function App() {
+  const {token, userId, login, logout} = useAuth()
   const routes = useRoutes(false);
   return (
     <Router>
