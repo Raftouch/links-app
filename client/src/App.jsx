c722f6426ec7c75c2a4bdfff98d2e8746574a41c
Raftouch <rafanelkin@gmail.com>
2023-05-09 17:39:37 +0200

    :rocket: created auth context, authorization done on frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,27 @@ import "materialize-css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { useRoutes } from "./routes/Routes";
 import { useAuth } from "./hooks/auth.hook";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
-  const {token, userId, login, logout} = useAuth()
-  const routes = useRoutes(false);
+  const { token, userId, login, logout } = useAuth();
+  const isAuthenticated = !!token;
+  const routes = useRoutes(isAuthenticated);
+
   return (
-    <Router>
-      <div className="container blue-text">{routes}</div>
-    </Router>
+    <AuthContext.Provider
+      value={{
+        token,
+        userId,
+        login,
+        logout,
+        isAuthenticated,
+      }}
+    >
+      <Router>
+        <div className="container blue-text">{routes}</div>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
