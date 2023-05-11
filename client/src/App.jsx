d74f6b83040f491afbd85747010c86418a31c721
Raftouch <rafanelkin@gmail.com>
2023-05-11 12:15:37 +0200

    :bug: fixed a bug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,16 +5,11 @@ import { useRoutes } from "./routes/Routes";
 import { useAuth } from "./hooks/auth.hook";
 import { AuthContext } from "./context/AuthContext";
 import { Navbar } from "./components/Navbar";
-import { Loader } from "./components/Loader";
 
 function App() {
-  const { token, userId, login, logout, ready } = useAuth();
+  const { token, userId, login, logout } = useAuth();
   const isAuthenticated = !!token;
   const routes = useRoutes(isAuthenticated);
-
-  if (!ready) {
-    return <Loader />;
-  }
 
   return (
     <AuthContext.Provider
